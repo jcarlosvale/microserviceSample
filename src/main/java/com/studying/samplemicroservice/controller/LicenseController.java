0a6feb7d75184d36c717ece83e884949fdaec40a
Joao Carlos Sousa do Vale <joaocarlos.sousadovale@tipico.com>
2022-03-23 22:05:18 +0100

    hateoas example in GET method

--- src/main/java/com/studying/samplemicroservice/controller/LicenseController.java
+++ src/main/java/com/studying/samplemicroservice/controller/LicenseController.java
@@ -1,5 +1,8 @@
 package com.studying.samplemicroservice.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import com.studying.samplemicroservice.model.License;
 import com.studying.samplemicroservice.service.LicenseService;
 import java.util.Locale;
@@ -24,7 +27,14 @@ public class LicenseController {
 
     @GetMapping(value="/{licenseId}")
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
+
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class).getLicense(organizationId, license.getLicenseId())).withSelfRel(),
+                linkTo(methodOn(LicenseController.class).createLicense(organizationId, license, null)).withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class).updateLicense(organizationId, license)).withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class).deleteLicense(organizationId, license.getLicenseId())).withRel("deleteLicense")
+        );
         return ResponseEntity.ok(license);
     }
 
